0cc06357b61ed5b30061c7e0b033b84754073870
Triston <tristonherbst97@gmail.com>
2021-03-23 14:43:15 -0500

    commit tree 445792b9d34dbb9222fda56896d36ab7c898a85f

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.destroy_all
+Genre.destroy_all
+Artist.destroy_all
+
+Artist.create(name:"T-money", bio: "likes to code")
+Artist.create(name:"nestor", bio: "good at coding")
+Artist.create(name:"jose", bio: "good coder")
+
+Genre.create(name:"pop")
+Genre.create(name:"hip")
+Genre.create(name:"hop")
+
+Song.create(name:"Dad in the house",artist: Artist.first , genre: Genre.first)
+Song.create(name:"song", artist: Artist.second , genre: Genre.third)
+Song.create(name:"blah", artist: Artist.third , genre: Genre.second)
+Song.create(name:"money", artist: Artist.first , genre: Genre.third)
+
